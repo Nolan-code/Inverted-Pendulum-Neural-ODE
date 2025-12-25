9e2b2c2e1b91b1eaf82851aee74d499c2d904bb0
Le Tyrant Nolan <nolanletyrant@gmail.com>
2025-12-25 20:18:34 +0100

    Delete analysis directory

--- analysis/dH_dt.py
+++
@@ -1,8 +0,0 @@
-def dH_dt(model, x):   # used to check if the HNN is hamiltonian (the output need to be close to 0)
-    x = x.clone().detach().requires_grad_(True)
-    dx = hamiltonian_dynamics_norm(model, x)
-
-    H = model((x - X_mean) / X_std).sum()
-    grad_H = torch.autograd.grad(H, x)[0]
-
-    return torch.dot(grad_H, dx)
--- analysis/mechanical_energy.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-
-def mechanical_energy(x, params):    # Verification that Em is roughly constant
-  m, l, g = params["m"], params["l"], params["g"]
-  theta = x[:,0]
-  omega = x[:,1]
-
-  E = 0.5 * m * l**2 * omega**2 + m * g * l * np.cos(theta)
-  return E
