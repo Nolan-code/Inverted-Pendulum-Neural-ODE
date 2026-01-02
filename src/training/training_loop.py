aaebe2b3942221ca8ca9aa50739f30718b95463b
Le Tyrant Nolan <nolanletyrant@gmail.com>
2026-01-02 16:22:42 +0100

    Create training_loop.py

---
+++ src/training/training_loop.py
@@ -0,0 +1,67 @@
+def build_model(model_name):
+    try:
+        return MODEL_REGISTRY[model_name]()
+    except KeyError:
+        raise ValueError(f"Unknown model type: {model_name}")
+
+#---------
+#  Model
+#---------
+
+model = build_model(args.model)
+optimizer = torch.optim.Adam(model.parameters(), lr=1e-3)
+loss_fn = nn.MSELoss()
+
+#----------------
+# Training loop
+#----------------
+
+train_loader, test_loader = load_pendulum(model_type="trig")
+n_epochs = 250
+
+for epoch in range(n_epochs):
+  model.train()
+  train_loss = 0.0
+
+  for X_batch, Y_batch in train_loader:
+
+    dx = model(X_batch)
+    dtheta_pred = dx[:,0]
+    domega_pred = dx[:,1]
+    loss = (
+        loss_fn(dtheta_pred, Y_batch[:,0]) +
+        loss_fn(domega_pred, Y_batch[:,1]) 
+    )
+
+    optimizer.zero_grad()
+    loss.backward()
+    optimizer.step()
+
+
+    train_loss += loss.item()
+
+  train_loss /= len(train_loader)
+
+
+  model.eval()
+  test_loss = 0.0
+
+  for X_batch, Y_batch in test_loader:
+
+      dx = model(X_batch)
+      dtheta_pred = dx[:,0]
+      domega_pred = dx[:,1]
+      loss = (
+        loss_fn(dtheta_pred, Y_batch[:,0]) +
+        loss_fn(domega_pred, Y_batch[:,1])
+      )
+      test_loss += loss.item()
+
+  test_loss /= len(test_loader)
+  print(f"Epoch {epoch+1}/{n_epochs}, Train Loss: {train_loss:.4f}, Test Loss: {test_loss:.4f}")
+
+#-------------
+# Save model
+#-------------
+
+torch.save(model.state_dict(), "HNN.pth")
