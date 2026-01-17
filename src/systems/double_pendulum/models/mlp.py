f68ccf691aa699eedf465bc1f55f776a8f7078f3
Le Tyrant Nolan <nolanletyrant@gmail.com>
2026-01-17 17:53:23 +0100

    Implement MLP class for neural network model

---
+++ src/systems/double_pendulum/models/mlp.py
@@ -0,0 +1,16 @@
+class MLP(nn.Module):
+  def __init__(self):
+    super().__init__()
+
+    self.net = nn.Sequential(
+        nn.Linear(6,64),
+        nn.Tanh(),
+        nn.Linear(64,64),
+        nn.Tanh(),
+        nn.Linear(64,64),
+        nn.Tanh(),
+        nn.Linear(64,4)
+    )
+
+  def forward(self,x):
+    return self.net(x)
